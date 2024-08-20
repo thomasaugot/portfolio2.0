7caf1c95a18ad491f59acdd3945795dab5d33b2e
THOMAS AUGOT <thomas.augot@hotmail.fr>
2024-08-20 19:30:31 +0200

    fxing loading component's gradient

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -13,15 +13,15 @@ const Loading: React.FC = () => {
       <StarsBackground />
       <div className="relative flex gap-4">
         <div className="w-12 h-12 rounded-full bg-primary-bg relative flex items-center justify-center">
-          <div className="absolute inset-0 rounded-full bg-gradient-button opacity-70 blur-lg animate-pulse"></div>
+          <div className="absolute inset-0 rounded-full bg-gradient opacity-70 blur-lg animate-pulse"></div>
           <div className="relative w-9 h-9 rounded-full bg-primary-bg animate-bounce"></div>
         </div>
         <div className="w-12 h-12 rounded-full bg-primary-bg relative flex items-center justify-center">
-          <div className="absolute inset-0 rounded-full bg-gradient-button opacity-70 blur-lg animate-pulse"></div>
+          <div className="absolute inset-0 rounded-full bg-gradient opacity-70 blur-lg animate-pulse"></div>
           <div className="relative w-9 h-9 rounded-full bg-primary-bg animate-bounce [animation-delay:-.3s]"></div>
         </div>
         <div className="w-12 h-12 rounded-full bg-primary-bg relative flex items-center justify-center">
-          <div className="absolute inset-0 rounded-full bg-gradient-button opacity-70 blur-lg animate-pulse"></div>
+          <div className="absolute inset-0 rounded-full bg-gradient opacity-70 blur-lg animate-pulse"></div>
           <div className="relative w-9 h-9 rounded-full bg-primary-bg animate-bounce [animation-delay:-.5s]"></div>
         </div>
       </div>
